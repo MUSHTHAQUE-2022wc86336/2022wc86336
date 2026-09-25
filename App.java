86b891a426241f94a2f6b912b31d930c0603f3d9
MUSHTHAQUE-2022wc86336 <2022wc86336@wilp.bits-pilani.ac.in>
2026-09-25 15:45:56 +0530

    Test Jenkins CI

--- App.java
+++ App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from DevOps Lab!");
+        System.out.println("Hello DevOps Jenkins CI test");
     }
 }
 
